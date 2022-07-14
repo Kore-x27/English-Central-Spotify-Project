61571939a3c7a2edbcfc7059fa43e830be4f2fc3
okurkcu <ozank2001@gmail.com>
2022-07-14 13:07:19 +0300

    creating login component
    creating login component

--- English-Central-Spotify-Project/src/app/app.module.ts
+++ English-Central-Spotify-Project/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { HttpClientModule } from '@angular/common/http';
+import { RedirectComponent } from './component/redirect/redirect.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    RedirectComponent,
+          LoginComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
