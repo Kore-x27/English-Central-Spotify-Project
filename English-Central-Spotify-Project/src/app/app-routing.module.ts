c2b57aa1a90d95f0ec5361ad0aa7ce645bf9fafe
Kore-x27 <popovdeniz@gmail.com>
2022-07-18 17:22:29 +0300

    Adding routing for Login

--- English-Central-Spotify-Project/src/app/app-routing.module.ts
+++ English-Central-Spotify-Project/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './component/login/login.component';
 import { AppComponent } from './app.component';
 import { RedirectComponent } from './component/redirect/redirect.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'redirect', component: RedirectComponent },
+  { path: 'login', component: LoginComponent}
+  //NEED TO CREATE A PATH FOR LOGIN COMPONENT (deniz)
 ];
 
 @NgModule({
